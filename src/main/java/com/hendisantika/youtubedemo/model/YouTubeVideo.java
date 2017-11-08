ad45f7f8a35617de4312a6bc57d2b8953a2c178a
Hendi Santika <hendisantika@gmail.com>
2017-11-09 04:09:02 +0700

    #2 - Added YouTube Video Model

---
+++ src/main/java/com/hendisantika/youtubedemo/model/YouTubeVideo.java
@@ -0,0 +1,62 @@
+package com.hendisantika.youtubedemo.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : candidate-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/11/17
+ * Time: 04.01
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class YouTubeVideo {
+
+    private String title;
+    private String url;
+    private String thumbnailUrl;
+    private String publishDate;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
